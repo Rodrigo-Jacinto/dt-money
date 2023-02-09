920745e28ef17fba4e515c4f419cbfbf76b50a2f
Rodrigo Jacinto <rodrigojacinto16@gmail.com>
2023-02-09 01:28:20 -0300

    feat: criação do componente tabela de transações

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransctionTable } from "../TransectionTable";
 import { Container } from "./style";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransctionTable />
     </Container>
   );
 }
---
+++ src/components/TransectionTable/index.tsx
@@ -0,0 +1,39 @@
+import { Container } from "./style";
+
+export function TransctionTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Preço</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento WEB SITE</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento WEB SITE</td>
+            <td className="deposit">R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento WEB SITE</td>
+            <td className="withdraw">- R$ 12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
